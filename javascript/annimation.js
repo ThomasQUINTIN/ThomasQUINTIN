a0b6ef0f974eb358b035b246a315af1e63ca390a
ThomasQUINTIN <thomas.quintin@epitech.eu>
2023-02-03 00:19:09 +0100

    💄 change the message bubble content

--- javascript/annimation.js
+++ javascript/annimation.js
@@ -1,9 +1,15 @@
 const targets = document.querySelectorAll('div');
 
+let element;
+
 function handleIntersection(entries) {
     entries.map((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible')
+            if (entry.target.classList[0] == 'talk-bubble')
+                entry.target.classList.add('also-visible')
+        } else if (entry.target.classList[5] != 'also-visible'){
+            entry.target.classList.remove('visible')
         }
     });
 }
